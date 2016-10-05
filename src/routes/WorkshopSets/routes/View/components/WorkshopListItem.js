070253cfb2b9707eee53e3c8bb206204bb511330
Jack Willis-Craig <jack.willis-craig@hotmail.com>
2016-10-06 10:37:48 +1100

    Add more info to workshop list.

--- src/routes/WorkshopSets/routes/View/components/WorkshopListItem.js
+++ src/routes/WorkshopSets/routes/View/components/WorkshopListItem.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import moment from 'moment'
 import { ListItem } from 'material-ui/List'
+
+import classes from './WorkshopListItem.scss'
 
 export default class WorkshopListItem extends React.Component {
 
@@ -24,6 +27,28 @@ export default class WorkshopListItem extends React.Component {
   }
 
   /**
+   * Constructs a date string for display.
+   *
+   * @param startDate
+   * @private
+   */
+  _getDateString(startDate) {
+    return moment(startDate).format('dddd, Do MMMM YYYY')
+  }
+
+  /**
+   * Constructs a time string for display.
+   *
+   * @param startDate
+   * @param endDate
+   * @returns {string}
+   * @private
+   */
+  _getTimeString(startDate, endDate) {
+    return moment(startDate).format('h:mma') + ' - ' + moment(endDate).format('h:mma')
+  }
+
+  /**
    * Renders the workshop set item.
    *
    * @returns {XML}
@@ -33,8 +58,37 @@ export default class WorkshopListItem extends React.Component {
 
     return (
       <ListItem
-        onTouchTap={this._onClick}
+        nestedItems={[
+          <div className={classes.moreInfo}>
+            <div className={classes.infoLine}>
+              <span className={classes.heading}>Places Available:</span>
+              <span className={classes.content}>{workshop.maximum - workshop.BookingCount}</span>
+            </div>
+            <div className={classes.infoLine}>
+              <span className={classes.heading}>Room:</span>
+              <span className={classes.content}>{workshop.campus}</span>
+            </div>
+            <div className={classes.infoLine}>
+              <span className={classes.heading}>Target Group:</span>
+              <span className={classes.content}>{workshop.targetingGroup}</span>
+            </div>
+            <div className={classes.infoLine}>
+              <span className={classes.heading}>What it Covers:</span>
+              <span className={classes.content}>{workshop.topic}</span>
+            </div>
+          </div>
+        ]}
+        initiallyOpen
         primaryText={workshop.topic}
+        primaryTogglesNestedList
+        secondaryText={
+          <p className={classes.secondaryText}>
+            {this._getDateString(workshop.StartDate)}
+            <br />
+            {this._getTimeString(workshop.StartDate, workshop.EndDate)}
+          </p>
+        }
+        secondaryTextLines={2}
       />
     )
   }
