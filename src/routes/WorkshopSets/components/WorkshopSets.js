a63e7e116b206e1d8ba30d24a1f86ac7bd7e3591
Jack Willis-Craig <jack.willis-craig@hotmail.com>
2016-10-10 21:18:00 +1100

    Remove subheader from workshop category list.

--- src/routes/WorkshopSets/components/WorkshopSets.js
+++ src/routes/WorkshopSets/components/WorkshopSets.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import Subheader from 'material-ui/Subheader'
 import { List } from 'material-ui/List'
 
 import WorkshopSetListItem from './WorkshopSetListItem'
 
 import classes from './WorkshopSets.scss'
-import strings from './WorkshopSets.strings'
 
 export default class WorkshopSets extends React.Component {
 
@@ -20,7 +18,6 @@ export default class WorkshopSets extends React.Component {
     return (
       <div className={classes.container}>
         <List>
-          <Subheader>{strings.list_title}</Subheader>
           {workshopSets.map(workshopSet => (
             <WorkshopSetListItem
               key={workshopSet.id}
